ea752ba72ebecbef9ccbd2c316a6888736ddb1d3
Jinglun Ma <2461447099ma@gmail.com>
2020-10-14 20:38:29 -0400

    Added return statements

--- Shapes.java
+++ Shapes.java
@@ -3,17 +3,16 @@ import java.util.*;
 public class Shapes{
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-    System.out.println("Choose the shape you want to calculate the area of. (square, triangle, rectangle, circle)");
-    String chose = input.next();
+    System.out.println("Choose the shape you want to calculate the area/volume of\n1.Square\n2.Triangle\n3.Rectangle\n4.Circle\n5.Cube\n6.Triangular Prism\n7.Rectangular Prism\n8.Sphere");
+    int chose = input.nextInt();
 
-    if ("square".equals(chose)) {
+    if (chose == 1) {
       System.out.print("Enter the side length: ");
       double side = input.nextDouble();
-      square(side);
-      System.out.println("Area of square is: " + sa);
+      System.out.println("Area of square is: " + square(side));
     } //close for square if
 
-    if ("triangle".equals(chose)) {
+    if (chose == 2) {
       System.out.println("Choose the way to calculate triangle. (1. base and height 2. 3 lengths)");
       int tAns = input.nextInt();
       if (tAns == 1) {
@@ -21,7 +20,7 @@ public class Shapes{
         double base = input.nextDouble();
         System.out.print("Enter the height of right triangle: ");
         double height = input.nextDouble();
-        triangle1(base,height);
+        System.out.println("Area of the triangle is: " + triangle1(base,height));
       } //close for if tAns 1
 
       if (tAns == 2) {
@@ -31,29 +30,28 @@ public class Shapes{
         double len2 = input.nextDouble();
         System.out.print("Enter the third length of triangle: ");
         double len3 = input.nextDouble();
-        triangle2(len1, len2, len3);
+        System.out.println("Area of the triangle is: " + triangle2(len1, len2, len3));
       } //close for if tAns 2
     } //close for triangles if
 
-    if ("rectangle".equals(chose)) {
+    if (chose == 3) {
       System.out.print("Enter the length of rectangle: ");
       double len = input.nextDouble();
       System.out.print("Enter the width of rectangle: ");
       double wid = input.nextDouble();
-      rectangle(len,wid);
+      System.out.println("Area of the rectangle is: " + rectangle(len,wid));
     } //end rectangle if
 
-    if ("circle".equals(chose)) {
+    if (chose == 4) {
       System.out.print("Enter the radius of circle: ");
       double r = input.nextDouble();
-      circle(r);
+      System.out.println("Area of the circle is: " + circle(r));
     } //end circle if
 
-    if ("cube".equals(chose)) {
+    if (chose == 5) {
       System.out.print("Enter the len of cube: ");
       double len = input.nextDouble();
-      square(len);
-      double cubeV = sa * len;
+      double cubeV = square(len) * len;
       System.out.println("Volume of cube is: " + cubeV);
     } //end cube if
 
@@ -68,7 +66,6 @@ public class Shapes{
 
   public static double triangle1(double base, double height) {
     double t1a = (base * height)/2;
-    System.out.println("Area of th right triangle is: " + t1a);
     return t1a;
   } //close for triangle1
 
@@ -76,20 +73,18 @@ public class Shapes{
     double s = (len1 + len2 + len3)/2;
     double area = s * (s - len1) * (s - len2) * (s - len3);
     double t2a = Math.pow(area, 0.5);
-    System.out.println("Area of the triangle is: " + t2a);
     return t2a;
   } //close for triangle2
 
   public static double rectangle(double len, double wid) {
     double ra = len * wid;
-    System.out.println("Area of the rectangle is: " + ra);
     return ra;
   } //end rectangle
 
-  public static double circle(double radius) {
-    double ca = radius * radius * Math.PI;
-    System.out.println("Area of the circle is: " + ca);
+  public static double circle(double r) {
+    double ca = r * r * Math.PI;
     return ca;
-  } //end circle
+  }
+
 
 } //close for class
